9c513b2986c82df01a3add3e0f30c14af5ebee67
DeniDias <denidias96@gmail.com>
2016-05-18 11:53:58 -0400

    Adding starter files of JasperXMLtoJB

---
+++ Attribute.java
@@ -0,0 +1,65 @@
+package deni.jasperxmltojb;
+
+import java.util.ArrayList;
+
+public class Attribute {
+	
+	private String type;
+	private String name;
+	private String method;
+	private ArrayList<String> getter;
+	private ArrayList<String> setter;
+	
+	public Attribute(){
+		getter = new ArrayList<>();
+		setter = new ArrayList<>();
+	}
+	
+	public String getType(){
+		return this.type;
+	}
+	
+	public String getMethod(){
+		return this.method;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public void setType(String type){
+		this.type = type;
+		if (name != null){
+			method = "get" + name.substring(0, 1).toUpperCase() + name.substring(1, name.length()) + "()";
+			method = "set" + name.substring(0, 1).toUpperCase() + name.substring(1, name.length()) + "("+type+" "+name+")";
+		}
+	}
+	
+	public void setName(String name){
+		this.name = name;
+		if (type != null){
+			method = "get" + name.substring(0, 1).toUpperCase() + name.substring(1, name.length()) + "()";
+			method = "set" + name.substring(0, 1).toUpperCase() + name.substring(1, name.length()) + "("+type+" "+name+")";
+		}
+	}
+	
+	public ArrayList<String> getGetterLines() throws Exception{
+		if (name == null){
+			throw new Exception("ERRO: Nome do atributo não definido.");
+		}
+		getter.add("	public "+type+" "+method+" {");
+		getter.add("		return "+name+";");
+		getter.add("	}");
+		return getter;
+	}
+	
+	public ArrayList<String> getSetterLines() throws Exception{
+		if (name == null){
+			throw new Exception("ERRO: Nome do atributo não definido.");
+		}
+		setter.add("	public void "+method+" {");
+		setter.add("		this."+name+" = "+" name");
+		setter.add("	}");
+		return setter;
+	}
+}
---
+++ JasperXMLtoJB.java
@@ -0,0 +1,154 @@
+package deni.jasperxmltojb;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class JasperXMLtoJB {
+
+	/* Captura o arquivo para efeituar a leitura */
+	private static FileReader fr;
+	
+	/* Leitura do arquivo JasperXML */
+	private static BufferedReader br;
+	
+	private static FileWriter fw;
+	
+	private static BufferedWriter bw;
+
+	private static ArrayList<String> fields;
+	
+	private static ArrayList<Attribute> attributes;
+	
+	private static Path java_file;
+	
+	private static Path function_file;
+
+	public static void main(String[] args) throws Exception {
+
+		/* Verifica se o arquivo foi passado por parametro */
+		if (args[0] == null) {
+			System.out.println("ERRO: Necessita de passar o arquivo .jrxml por parâmetro");
+			return;
+		}
+
+		/* Verifica se o nome da classe foi passada por parametro */
+		if (args[1] == null) {
+			System.out.println("ERRO: Necessita de passar o nome da classe por parâmetro");
+			return;
+		}
+
+		/* Verifica se o arquivo passado é JRXML */
+		if (!args[0].substring(args[0].length() - 6, args[0].length()).equals(".jrxml")) {
+			System.out.println("ERRO: Arquivo não é JRXML");
+			return;
+		}
+
+		/* Verifica se o arquivo existe */
+		try {
+			fr = new FileReader(args[0]);
+			br = new BufferedReader(fr);
+			fields = new ArrayList<>();
+			attributes = new ArrayList<>();
+		} catch (FileNotFoundException e) {
+			System.out.println("ERRO: Arquivo não encontrado.");
+		}
+
+		/* Efetua a leitura dos arquivos */
+		String line = br.readLine();
+		while (line != null) {
+			if (line.contains("<field")) {
+				fields.add(line);
+				// System.out.println(line);
+			}
+			line = br.readLine();
+		}
+
+		for (String aux : fields) {
+			String[] params = aux.split(" ");
+			Attribute at = new Attribute();
+			for (int i = 0; i < params.length; i++) {
+				if (params[i].contains("name=")) {
+					String[] params_name = params[i].split("\"");
+					//System.out.println(params_name[1]);
+					at.setName(params_name[1]);
+				} else if (params[i].contains("class=")) {
+					String[] params_class = params[i].split("\"");
+					//System.out.println(params_class[1]);
+					int last_type_index = params_class[1].lastIndexOf('.') + 1;
+					at.setType(params_class[1].substring(last_type_index));
+				} 
+				
+				if (i == params.length-1){
+					attributes.add(at);
+				}
+			}
+		}
+		
+		java_file = new File(args[1]+".java").toPath();
+		
+		if (Files.exists(java_file)) {
+			Files.delete(java_file);
+		}
+		
+		Files.createFile(java_file);
+		
+		
+		fw = new FileWriter(args[1]+".java");
+		bw = new BufferedWriter(fw);
+		
+		bw.newLine();
+		bw.newLine();
+		bw.write("public class "+args[1]+" {");
+		bw.newLine();
+		bw.newLine();
+		
+		for (Attribute at : attributes){
+			bw.write("	private "+at.getType()+" "+at.getName()+";");
+			bw.newLine();
+		}
+		
+		bw.newLine();
+		
+		for (Attribute at : attributes){
+			for (String lines : at.getGetterLines()){
+				bw.write(lines);
+				bw.newLine();
+			}
+			
+			for (String lines : at.getSetterLines()){
+				bw.write(lines);
+				bw.newLine();
+			}
+		}
+		
+		bw.write("}");
+		bw.flush();
+		
+		function_file = new File("functions.txt").toPath();
+		
+		if (Files.exists(function_file)) {
+			Files.delete(function_file);
+		}
+		
+		Files.createFile(function_file);
+		
+		fw = new FileWriter("functions.txt");
+		bw = new BufferedWriter(fw);
+		
+		for (Attribute at : attributes){
+			bw.write(at.getMethod());
+			bw.newLine();
+		}
+		
+		bw.flush();
+		
+		System.out.println("Operação realizada com sucesso!");
+	}
+}
